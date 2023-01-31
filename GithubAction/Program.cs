789762508d68bd941e98c2ce54a94c1b07be0585
timmoth <t.jones@timmoth.com>
2023-01-31 21:27:35 +0000

    initial commit

---
+++ GithubAction/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using CommandLine;
+using GithubAction;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using static CommandLine.Parser;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) => { })
+    .Build();
+
+static TService Get<TService>(IHost host)
+    where TService : notnull =>
+    host.Services.GetRequiredService<TService>();
+
+var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
+parser.WithNotParsed(
+    errors =>
+    {
+        Get<ILoggerFactory>(host)
+            .CreateLogger("GithubActions")
+            .LogError(
+                string.Join(
+                    Environment.NewLine, errors.Select(error => error.ToString())));
+
+        Environment.Exit(2);
+    });
+
+await parser.WithParsedAsync(
+    async options => await StartAnalysisAsync(options, host));
+await host.RunAsync();
+
+static async ValueTask StartAnalysisAsync(ActionInputs inputs, IHost host)
+{
+    var updatedMetrics = true;
+    var title = "Updated 2 projects";
+    var summary = "Calculated code metrics on two projects.";
+
+    // Write GitHub Action workflow outputs.
+    var gitHubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+    if (!string.IsNullOrWhiteSpace(gitHubOutputFile))
+    {
+        using StreamWriter textWriter = new(gitHubOutputFile, true, Encoding.UTF8);
+        textWriter.WriteLine($"updated-metrics={updatedMetrics}");
+        textWriter.WriteLine($"summary-title={title}");
+        textWriter.WriteLine($"summary-details={summary}");
+    }
+
+    await ValueTask.CompletedTask;
+
+    Environment.Exit(0);
+}
