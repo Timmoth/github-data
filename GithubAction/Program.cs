8830e3b74c02981e76334c4e2e8c000a1345a0f0
timmoth <t.jones@timmoth.com>
2023-02-01 18:22:09 +0000

    Added logging

--- GithubAction/Program.cs
+++ GithubAction/Program.cs
@@ -7,38 +7,38 @@ using Microsoft.Extensions.Logging;
 using static CommandLine.Parser;
 
 using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) => { })
+    .ConfigureServices((_, services) => { services.AddLogging(); })
     .Build();
-
-static TService Get<TService>(IHost host)
-    where TService : notnull =>
-    host.Services.GetRequiredService<TService>();
 
 var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
 parser.WithNotParsed(
     errors =>
     {
-        Get<ILoggerFactory>(host)
-            .CreateLogger("GithubActions")
-            .LogError(
+        host.GetLogger().LogError(
                 string.Join(
                     Environment.NewLine, errors.Select(error => error.ToString())));
 
         Environment.Exit(2);
     });
 
-await parser.WithParsedAsync(
-    async options => await StartAnalysisAsync(options, host));
+await parser.WithParsedAsync(async options => await StartAnalysisAsync(options, host));
 await host.RunAsync();
 
-static async ValueTask StartAnalysisAsync(ActionInputs inputs, IHost host)
+static async Task StartAnalysisAsync(ActionInputs inputs, IHost host)
 {
+    var logger = host.GetLogger();
+
+    logger.LogInformation("Begin");
+
     var updatedMetrics = true;
     var title = "Updated 2 projects";
     var summary = "Calculated code metrics on two projects.";
 
     // Write GitHub Action workflow outputs.
     var gitHubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+
+    logger.LogInformation($"GITHUB_OUTPUT: '{gitHubOutputFile}'");
+
     if (!string.IsNullOrWhiteSpace(gitHubOutputFile))
     {
         using StreamWriter textWriter = new(gitHubOutputFile, true, Encoding.UTF8);
@@ -47,7 +47,7 @@ static async ValueTask StartAnalysisAsync(ActionInputs inputs, IHost host)
         textWriter.WriteLine($"summary-details={summary}");
     }
 
-    await ValueTask.CompletedTask;
-
+    logger.LogInformation("End");
+    await Task.Delay(TimeSpan.FromMilliseconds(1));
     Environment.Exit(0);
 }
---
+++ GithubAction/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public static class ServiceExtensions
+{
+    public static TService Get<TService>(this IHost host)
+    where TService : notnull =>
+    host.Services.GetRequiredService<TService>();
+
+    public static ILogger GetLogger(this IHost host) => host.Get<ILoggerFactory>().CreateLogger("Github Action");
+}
