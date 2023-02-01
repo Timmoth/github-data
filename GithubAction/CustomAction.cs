759cf4b64b2acfc60317371c067e5ce86814a87a
timmoth <t.jones@timmoth.com>
2023-02-01 18:48:59 +0000

    Cleaned up program.cs

---
+++ GithubAction/CustomAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using GithubAction;
+using Microsoft.Extensions.Logging;
+
+public sealed class CustomAction
+{
+    private readonly ILogger<CustomAction> logger;
+    private readonly ActionInputs actionInputs;
+
+    public CustomAction(ILogger<CustomAction> logger, ActionInputs actionInputs)
+    {
+        this.logger = logger;
+        this.actionInputs = actionInputs;
+    }
+
+    public Task Run()
+    {
+        logger.LogInformation("Begin");
+        logger.LogInformation($"Name: '{actionInputs.Name}'");
+
+        var updatedMetrics = true;
+        var title = "Updated 2 projects";
+        var summary = "Calculated code metrics on two projects.";
+
+        // Write GitHub Action workflow outputs.
+        var gitHubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+
+        logger.LogInformation($"GITHUB_OUTPUT: '{gitHubOutputFile}'");
+
+        if (!string.IsNullOrWhiteSpace(gitHubOutputFile))
+        {
+            using StreamWriter textWriter = new(gitHubOutputFile, true, Encoding.UTF8);
+            textWriter.WriteLine($"updated-metrics={updatedMetrics}");
+            textWriter.WriteLine($"summary-title={title}");
+            textWriter.WriteLine($"summary-details={summary}");
+        }
+
+        logger.LogInformation("End");
+        return Task.CompletedTask;
+    }
+}
--- GithubAction/Program.cs
+++ GithubAction/Program.cs
@@ -1,53 +1,26 @@
-﻿using System.Text;
-using CommandLine;
-using GithubAction;
+﻿using GithubAction;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using static CommandLine.Parser;
 
+var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
+if (parser.Errors.Any())
+{
+    Console.Error.WriteLine(string.Join(
+            Environment.NewLine, parser.Errors.Select(error => error.ToString())));
+    Environment.Exit(2);
+}
+
 using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) => { services.AddLogging(); })
+    .ConfigureServices((_, services) => { 
+        services.AddLogging();
+        services.AddScoped<CustomAction>();
+        services.AddSingleton(parser.Value);
+    })
     .Build();
 
-var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
-parser.WithNotParsed(
-    errors =>
-    {
-        host.GetLogger().LogError(
-                string.Join(
-                    Environment.NewLine, errors.Select(error => error.ToString())));
-
-        Environment.Exit(2);
-    });
-
-await parser.WithParsedAsync(async options => await StartAnalysisAsync(options, host));
-await host.RunAsync();
-
-static async Task StartAnalysisAsync(ActionInputs inputs, IHost host)
-{
-    var logger = host.GetLogger();
-
-    logger.LogInformation("Begin");
-
-    var updatedMetrics = true;
-    var title = "Updated 2 projects";
-    var summary = "Calculated code metrics on two projects.";
-
-    // Write GitHub Action workflow outputs.
-    var gitHubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
-
-    logger.LogInformation($"GITHUB_OUTPUT: '{gitHubOutputFile}'");
-
-    if (!string.IsNullOrWhiteSpace(gitHubOutputFile))
-    {
-        using StreamWriter textWriter = new(gitHubOutputFile, true, Encoding.UTF8);
-        textWriter.WriteLine($"updated-metrics={updatedMetrics}");
-        textWriter.WriteLine($"summary-title={title}");
-        textWriter.WriteLine($"summary-details={summary}");
-    }
-
-    logger.LogInformation("End");
-    await Task.Delay(TimeSpan.FromMilliseconds(1));
-    Environment.Exit(0);
-}
+using IServiceScope serviceScope = host.Services.CreateScope();
+IServiceProvider provider = serviceScope.ServiceProvider;
+var customAction = provider.GetRequiredService<CustomAction>();
+await customAction.Run();
+Environment.Exit(0);
